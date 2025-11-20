0e55303e64ac4bbc4450d0390164354ccdc66abc
linerol <sessi.tchecounnou@epitech.eu>
2025-11-20 12:16:42 +0100

    Change default port from 4000 to 3000 in main application file

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   });
   configureApp(app);
 
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on port: ${port}`);
 }
